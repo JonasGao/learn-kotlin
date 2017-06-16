15555b55cf2ab3d25c1a31ae108712330bbf5265
JonasGao <jonas.gao@voyageone.cn>
2017-06-16 15:11:46 +0800

    add: destructure

--- src/main/kotlin/com/jonas/test/kotlin/Destructure.kt
+++ src/main/kotlin/com/jonas/test/kotlin/Destructure.kt
@@ -8,4 +8,12 @@ fun main(args: Array<String>) {
     val (_, red, green, blue) = match!!.groupValues
     println("$red, $green, $blue")
     println(red.toInt(16))
+
+    val person = Person("Jonas", 18)
+    val (age, name) = person
+
+    println(name)
+    println(age)
 }
+
+data class Person(val name: String, val age: Int)
